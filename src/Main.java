aa9719b0f63e992901085e2b55363b789a0cbf9b
rfx153 <rafaelfx153@gmail.com>
2025-02-15 17:27:20 -0300

    Imprima o tamanho da lista utilizando o método size.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
             jose.setIdade(25);
             jose.setNome("josé silva");
             System.out.println(jose.toString());
+
             //adicionando novas pessoas
             //Mario
             Pessoa mario = new Pessoa();
@@ -26,6 +27,14 @@ public class Main {
             sonia.setNome("Sonia");
             sonia.setIdade(45);
             System.out.println(sonia.toString());
+
+            //adicionando as pessoas a lista
+            listaDePessoas.add(jose);
+            listaDePessoas.add(maria);
+            listaDePessoas.add(mario);
+            listaDePessoas.add(sonia);
+            //imprimindo o tamanho da lista
+            System.out.println("o tamano da lista é:" + listaDePessoas.size());
 
     }
 }
