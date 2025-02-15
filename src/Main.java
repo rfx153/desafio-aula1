106e50e783421f0d112936d11091aa229fdcfe04
rfx153 <rafaelfx153@gmail.com>
2025-02-15 17:11:36 -0300

    terminei o item 2 No método main da classe Principal, crie um ArrayList de Pessoa chamado listaDePessoas.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+            ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
             Pessoa jose = new Pessoa();
 
             jose.setIdade(25);
