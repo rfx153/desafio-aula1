ab6e55e307ca24be8541105f3dbca30d4a2eba0d
rfx153 <rafaelfx153@gmail.com>
2025-02-15 17:05:43 -0300

    primeiro item feito

---
+++ src/Pessoa.java
@@ -0,0 +1,25 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "o nome dessa pessoa é: " +this.getNome()+ " e ela tem " + String.valueOf(this.getIdade()) + " de idade";
+    }
+}
